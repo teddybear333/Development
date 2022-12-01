8fc704ddd5c524b165f664b86d558915853ebba7
teddybear333 <114889634+teddybear333@users.noreply.github.com>
2022-11-30 23:06:22 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
   return (
     <div id="app">
       <header>
-        <h2> ✿ Welcome to my Anime Database ✿ </h2>
+        <h2> ✨ Welcome to Anime Base ✨ </h2>
       </header>
       <main>
         <div id="left-section">
